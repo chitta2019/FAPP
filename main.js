0e7a17517ca67a2e549ee750ba35c0780f422748
chitta2019 <chitta2019@gmail.com>
2017-01-23 23:39:14 +0530

    Website

---
+++ main.js
@@ -0,0 +1,12 @@
+function seeData(){
+	
+	var x = document.getElementById("name").value;
+	
+	var y = document.getElementById("en").value;
+
+	var z = document.getElementById("occ").value;
+
+	document.write("Name = " + x + "<br/>" + "Enrollment Number = " + y + "<br/>" + "Occassion = " + z); 
+
+	
+}
